cc0016b6555258a61ccd5994b659ebdb576b16e4
Daniel Ferreira Gómez <ferreiragomezdaniel@gmail.com>
2023-06-23 16:03:03 +0200

    Test

--- webapp/src/app/core/services/admin/api.admin.service.ts
+++ webapp/src/app/core/services/admin/api.admin.service.ts
@@ -19,6 +19,7 @@ export class ApiService {
   constructor(private http: HttpClient) { }
 
   private handleError(error: HttpErrorResponse) {
+    console.log("idjhfleiudjhflijsadhfgoqwuijehfgoieujqhfoduijhfosdjfhsodijuhfweriqoujhfoweijhfoweiudjfhnqweoruihfoqweiufhqweiujfhodujhfiosdqujfhqf")
     if (error.status === 0) {
       // A client-side or network error occurred. Handle it accordingly.
       console.error('An error occurred:', error.error);
