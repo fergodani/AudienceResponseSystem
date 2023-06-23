fa095075c3f091a8b45a0f53287ddbbcd085e98b
Daniel Ferreira Gómez <ferreiragomezdaniel@gmail.com>
2023-06-23 16:07:32 +0200

    test

--- webapp/src/app/core/services/admin/api.admin.service.ts
+++ webapp/src/app/core/services/admin/api.admin.service.ts
@@ -19,7 +19,6 @@ export class ApiService {
   constructor(private http: HttpClient) { }
 
   private handleError(error: HttpErrorResponse) {
-    console.log("idjhfleiudjhflijsadhfgoqwuijehfgoieujqhfoduijhfosdjfhsodijuhfweriqoujhfoweijhfoweiudjfhnqweoruihfoqweiufhqweiujfhodujhfiosdqujfhqf")
     if (error.status === 0) {
       // A client-side or network error occurred. Handle it accordingly.
       console.error('An error occurred:', error.error);
@@ -34,6 +33,7 @@ export class ApiService {
   }
 
   createUser(user: User): Observable<Message> {
+    console.log("createUser")
     return this.http.post<Message>(`${environment.apiUrl}/user/create`, user)
       .pipe(
         catchError(this.handleError)
@@ -41,6 +41,7 @@ export class ApiService {
   }
 
   createCourse(course: Course): Observable<Course> {
+    console.log("createCourse")
     return this.http.post<Course>(`${environment.apiUrl}/course`, course)
       .pipe(
         catchError(this.handleError)
@@ -48,6 +49,7 @@ export class ApiService {
   }
 
   getUsers(): Observable<User[]> {
+    console.log("getUsers")
     return this.http.get<User[]>(`${environment.apiUrl}/user`)
       .pipe(
         catchError(this.handleError)
@@ -55,6 +57,7 @@ export class ApiService {
   }
 
   getUser(id: number): Observable<User> {
+    console.log("getUser")
     return this.http.get<User>(`${environment.apiUrl}/user/${id}`)
       .pipe(
         catchError(this.handleError)
@@ -62,6 +65,7 @@ export class ApiService {
   }
 
   getCourses(): Observable<Course[]> {
+    console.log("getCourses")
     return this.http.get<Course[]>(`${environment.apiUrl}/course`)
       .pipe(
         catchError(this.handleError)
@@ -69,6 +73,7 @@ export class ApiService {
   }
 
   getCourse(id: number): Observable<Course> {
+    console.log("getCourse")
     return this.http.get<Course>(`${environment.apiUrl}/course/${id}`)
       .pipe(
         catchError(this.handleError)
@@ -76,6 +81,7 @@ export class ApiService {
   }
 
   deleteUser(id: number): Observable<Message> {
+    console.log("deleteUser")
     return this.http.delete<Message>(`${environment.apiUrl}/user/${id}`)
       .pipe(
         catchError(this.handleError)
@@ -83,6 +89,7 @@ export class ApiService {
   }
 
   deleteCourse(id: number): Observable<unknown> {
+    console.log("deleteCourse")
     return this.http.delete(`${environment.apiUrl}/course/${id}`)
       .pipe(
         catchError(this.handleError)
@@ -90,7 +97,7 @@ export class ApiService {
   }
 
   updateUser(user: User): Observable<Message> {
-    console.log(user)
+    console.log("updateUser")
     return this.http.put<Message>(`${environment.apiUrl}/user`, user)
       .pipe(
         catchError(this.handleError)
@@ -98,6 +105,7 @@ export class ApiService {
   }
 
   updateCourse(course: Course): Observable<Course> {
+    console.log("updateCourse")
     return this.http.put<Course>(`${environment.apiUrl}/course`, course)
       .pipe(
         catchError(this.handleError)
@@ -105,6 +113,7 @@ export class ApiService {
   }
 
   uploadUserFile(formData: FormData): Observable<Message> {
+    console.log("uploadUserFile")
     return this.http.post<Message>(`${environment.apiUrl}/user/file`, formData)
       .pipe(
         catchError(this.handleError)
@@ -112,6 +121,7 @@ export class ApiService {
   }
   
   uploadCourseFile(formData: FormData): Observable<unknown> {
+    console.log("uploadCourseFile")
     return this.http.post<unknown>(`${environment.apiUrl}/course/file`, formData)
       .pipe(
         catchError(this.handleError)
@@ -119,6 +129,7 @@ export class ApiService {
   }
 
   addUserToCourse(userCourse: UserCourse): Observable<unknown> {
+    console.log("addUserToCourse")
     return this.http.post<UserCourse>(`${environment.apiUrl}/course/addUser`, userCourse)
     .pipe(
       catchError(this.handleError)
@@ -126,6 +137,7 @@ export class ApiService {
   }
 
   addSurveyToCourse(surveyCourse: SurveyCourse): Observable<unknown> {
+    console.log("addSurveyToCourse")
     return this.http.post<SurveyCourse>(`${environment.apiUrl}/course/addSurvey`, surveyCourse)
     .pipe(
       catchError(this.handleError)
@@ -133,6 +145,7 @@ export class ApiService {
   }
 
   addQuestionToCourse(questionCourse: QuestionCourse): Observable<Message> {
+    console.log("addQuestionToCourse")
     return this.http.post<Message>(`${environment.apiUrl}/course/addQuestion`, questionCourse)
     .pipe(
       catchError(this.handleError)
